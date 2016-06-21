17c7c741b369f1b8b1d14616f6adad5011a91b1a
kevalMD <keval.multidots@gmail.com>
2016-06-21 12:13:58 +0530

    Google Plus, Twitter, LinkedIn and facebook signin

---
+++ app/src/main/java/login/social/sample/googleAuthSignin/GoogleSignInHelper.java
@@ -0,0 +1,16 @@
+package login.social.sample.googleAuthSignin;
+
+import android.content.Context;
+
+/**
+ * Created by multidots on 6/21/2016.
+ * This is the helper class for Google sign in using Google Auth.
+ * You can find detail steps here: {@link 'https://developers.google.com/identity/sign-in/android/start-integrating#add-config'}
+ */
+public class GoogleSignInHelper {
+
+    public GoogleSignInHelper(Context context){
+
+    }
+
+}
---
+++ app/src/main/java/login/social/sample/linkedInSiginIn/LinkedInUser.java
@@ -0,0 +1,16 @@
+package login.social.sample.linkedInSiginIn;
+
+/**
+ * Created by multidots on 6/17/2016.<p>
+ * This class represents linked in user profile.
+ */
+public class LinkedInUser {
+
+    public String name;
+
+    public String email;
+
+    public String phone;
+
+    public String pictureUrl;
+}
